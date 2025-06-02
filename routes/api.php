8ec247fe0aa2079ee1034d6012db3a2b4b1002f9
Vinicius Fazzano <viniciusg8justinfazzano@gmail.com>
2025-06-02 19:38:46 -0400

    ajustes de diretorios de teste

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,3 @@
 
 //inclui rotas padroes da api
 include __DIR__.'/api/v1/users.php';
-include __DIR__.'/api/v1/url.php';
-include __DIR__.'/api/v1/questionario.php';
-include __DIR__.'/api/v1/questionarioCoordenador.php';
-include __DIR__.'/api/v1/questionarioAvaliador.php';
-include __DIR__.'/api/v1/principio.php';
-include __DIR__.'/api/v1/relatorios.php';
